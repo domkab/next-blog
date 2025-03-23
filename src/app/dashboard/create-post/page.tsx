6218d64ba5e55344e5e52a850d0bb16fdc7dee07
domkacore <dominykas.bobinas@gmail.com>
2025-03-23 17:32:23 +0200

    crazy error ?

--- src/app/dashboard/create-post/page.tsx
+++ src/app/dashboard/create-post/page.tsx
@@ -6,15 +6,13 @@ import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { useState } from 'react';
 import 'react-quill-new/dist/quill.snow.css';
-
-const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
-
 import { PostCategory } from '@/types';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import axios from 'axios';
 import Link from 'next/link';
 import usePostForm from '@/app/hooks/usePostForm';
+const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 
 export default function CreatePostPage() {
   const { isSignedIn, user, isLoaded } = useUser();
