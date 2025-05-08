ee3667be6f1f974ee2c1c5d986dca212ea28eba3
domkab <dominykas.bobinas@gmail.com>
2025-05-08 17:00:38 +0300

    homepage redo :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
 
   return (
     <div className='flex flex-col justify-center items-center'>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+      {/* <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
         <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
         <p className='text-gray-500 text-sm sm:text-base'>
           Discover a variety of articles and tutorials on topics such as web
@@ -50,7 +50,7 @@ export default async function Home() {
         >
           View all posts
         </Link>
-      </div>
+      </div> */}
       <div className='p-3 bg-amber-100 dark:bg-slate-700'>
         <CallToAction />
       </div>
