4bfd99d95f075957c7f0726cd4b861b88715b9df
domkab <dominykas.bobinas@gmail.com>
2025-05-14 14:19:30 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import CallToAction from './components/CallToAction';
 import RecentPosts from './components/RecentPosts';
-// import axios from 'axios';
+import CallToAction from './components/CallToAction2';
+import FeaturedPost from './components/FeaturedPost';
 
 export default async function Home() {
   // let posts = null;
@@ -18,7 +18,7 @@ export default async function Home() {
   //     },
 
   //   )
-    
+
   // } catch (error) {
   //   if (axios.isCancel(error)) {
   //     console.log('Request canceled,', error.message);
@@ -28,19 +28,66 @@ export default async function Home() {
   // }
 
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+    <main className="flex flex-col justify-center items-center">
+      <section className='w-full' aria-label="Featured Post">
+        <FeaturedPost />
+      </section>
+
+      <section aria-label="Subscribe to Newsletter">
         <CallToAction />
-      </div>
-      <div className='p-3 flex flex-col gap-8 py-7'>
+      </section>
+
+      <section className="p-3 flex flex-col gap-8 py-7" aria-label="Recent Articles">
         <RecentPosts limit={9} />
         <Link
           href={'/search?category=null'}
-          className='text-lg text-teal-500 hover:underline text-center'
+          className="text-lg text-teal-500 hover:underline text-center"
         >
           View all posts
         </Link>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
+
+
+// export default async function Home() {
+//   // let posts = null;
+
+//   // const controller = new AbortController();
+
+//   // try {
+//   //   const { data } = await axios.post(
+//   //     `${process.env.NEXT_PUBLIC_URL}/api/post/get`,
+//   //     { limit: 9, order: 'desc' },
+//   //     {
+//   //       headers: { 'Cache-control': 'no-store' },
+//   //       signal: controller.signal
+//   //     },
+
+//   //   )
+
+//   // } catch (error) {
+//   //   if (axios.isCancel(error)) {
+//   //     console.log('Request canceled,', error.message);
+//   //   }
+
+//   //   console.log('Error getting post:', error);
+//   // }
+
+//   return (
+//     <div className='flex flex-col justify-center items-center'>
+//       {/* <CallToAction /> */}
+//       <CallToAction2 />
+//       <div className='p-3 flex flex-col gap-8 py-7'>
+//         <RecentPosts limit={9} />
+//         <Link
+//           href={'/search?category=null'}
+//           className='text-lg text-teal-500 hover:underline text-center'
+//         >
+//           View all posts
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
