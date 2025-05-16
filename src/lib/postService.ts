72f800870b8240c180f07dd11f14a39bc4778079
domkab <dominykas.bobinas@gmail.com>
2025-05-16 18:47:19 +0300

    added internal service for recent posts + test in vercel

---
+++ src/lib/postService.ts
@@ -0,0 +1,18 @@
+import { connect } from '@/lib/mongodb/mongoose';
+import Post from './models/postModel';
+import { PostType } from '@/types/Post';
+
+export async function getRecentPosts(limit = 9, order = 'desc'): Promise<PostType[]> {
+  await connect();
+  const sort = order === 'asc' ? 1 : -1;
+
+  const posts = await Post.find().lean().sort({ updatedAt: sort }).limit(limit);
+
+  return posts as PostType[]
+}
+
+export async function getPostBySlug(slug: string) {
+  await connect();
+
+  return await Post.findOne({ slug }).lean();
+}
