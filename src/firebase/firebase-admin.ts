5529fd587c3aa0f1f46983bb14a995ad6ca2012e
domkab <dominykas.bobinas@gmail.com>
2025-06-01 12:59:17 +0300

    improving firebase url paths and protectins

--- src/firebase/firebase-admin.ts
+++ src/firebase/firebase-admin.ts
@@ -1,10 +1,14 @@
-import { initializeApp, getApps, applicationDefault } from 'firebase-admin/app';
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getStorage } from 'firebase-admin/storage';
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64!, 'base64').toString('utf8')
+);
 
 if (!getApps().length) {
   initializeApp({
-    credential: applicationDefault(),
-    storageBucket: 'next-blog-acbbc.firebasestorage.app',
+    credential: cert(serviceAccount),
+    storageBucket: "next-blog-acbbc.appspot.com",
   });
 }
 
