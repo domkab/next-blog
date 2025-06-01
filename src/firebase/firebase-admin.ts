fe7d90705c2bbc709071abbf875950bc49e3dc92
domkab <dominykas.bobinas@gmail.com>
2025-06-01 14:32:20 +0300

    update docker compose env, clean up files

--- src/firebase/firebase-admin.ts
+++ src/firebase/firebase-admin.ts
@@ -1,14 +1,14 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getStorage } from 'firebase-admin/storage';
 
-const serviceAccount = JSON.parse(
+const decoded = JSON.parse(
   Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64!, 'base64').toString('utf8')
 );
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
-    storageBucket: "next-blog-acbbc.appspot.com",
+    credential: cert(decoded),
+    storageBucket: 'next-blog-acbbc.firebasestorage.app',
   });
 }
 
