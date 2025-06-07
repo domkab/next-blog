66a60588ebfe3404e7dfbb4eccbb7592686da68e
domkab <dominykas.bobinas@gmail.com>
2025-06-07 17:13:55 +0300

    update

--- src/app/components/FeaturedPost/FeaturedPost.tsx
+++ src/app/components/FeaturedPost/FeaturedPost.tsx
@@ -2,7 +2,7 @@ import { getFeaturedPosts } from '@/lib/services/postService';
 import LinkTracker from '../Tracking/LinkTracker';
 import SecuredImage from '../SecureImage';
 
-export const revalidate = Number(process.env.CACHE_REVALIDATION_INTERVAL);
+export const revalidate = Number(process.env.CACHE_REVALIDATION_INTERVAL) || 120;
 
 export default async function FeaturedPost() {
   const featured = await getFeaturedPosts();
