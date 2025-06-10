690d4eb393bd99ef7ad6bbf80678b436a8464200
domkab <dominykas.bobinas@gmail.com>
2025-06-10 18:31:50 +0300

    setup privacy policy and cookies page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,4 @@
-export const SITE_TITLE = 'Site Title';
+export const SITE_TITLE = 'Site Title Example';
+export const SITE_NAME = 'example';
+export const DOMAIN_NAME = 'example.com';
 export const CHARACTER_NAME = 'Pixel';
