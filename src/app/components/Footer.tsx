8b4f19e522c0ea164802a3d2e2c98ea6582f2bac
domkab <dominykas.bobinas@gmail.com>
2025-06-24 19:53:07 +0300

    updated footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -21,36 +21,30 @@ export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
       <div className='w-full max-w-7xl mx-auto'>
-        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+        <div className='grid w-full md:justify-between sm:flex md:grid-cols-1'>
           <div className='mt-5'>
             <Logo />
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
             <div>
-              <FooterTitle title='About' />
+              <FooterTitle title="Blog" />
               <FooterLinkGroup col>
-                <FooterLink
-                  href='/about'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Consumer&apos;s Prices
-                </FooterLink>
+                <FooterLink href="/about">About Us</FooterLink>
+                <FooterLink href="/contact">Contact Us</FooterLink>
               </FooterLinkGroup>
             </div>
-
-            <div>
-              <FooterTitle title='Contact' />
-              <FooterLinkGroup col>
-                <FooterLink href='/contact'>Contact Us</FooterLink>
-              </FooterLinkGroup>
-            </div>
-
             <div>
               <FooterTitle title='Legal' />
               <FooterLinkGroup col>
-                <FooterLink href='/privacy-policy'>Privacy & Cookies</FooterLink>
-                <FooterLink href='terms-conditions'>Terms &amp; Conditions</FooterLink>
+                <FooterLink href='/privacy-policy'>
+                  Privacy&nbsp;&amp;&nbsp;Cookies
+                </FooterLink>
+                <FooterLink href='/terms-conditions'>
+                  Terms&nbsp;&amp;&nbsp;Conditions
+                </FooterLink>
+                <FooterLink href='/privacy-controls'>
+                  Do&nbsp;Not&nbsp;Sell&nbsp;/&nbsp;Share&nbsp;My&nbsp;Info
+                </FooterLink>
               </FooterLinkGroup>
             </div>
           </div>
