a3571a45810ea77929eda301fa678dc5810027d8
domkab <dominykas.bobinas@gmail.com>
2025-06-28 16:17:58 +0300

    subscribe modal standalone + footer changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,20 +4,21 @@ import {
   Footer,
   FooterCopyright,
   FooterDivider,
-  FooterIcon,
   FooterLink,
   FooterLinkGroup,
   FooterTitle,
+  Modal,
 } from 'flowbite-react';
-import {
-  BsFacebook,
-  BsInstagram,
-  BsTwitter,
-  BsGithub,
-  BsDribbble,
-} from 'react-icons/bs';
 import Logo from './Logo';
+import { BsEnvelope } from 'react-icons/bs';
+
+import { useState } from 'react';
+import EmailSubscribeModalAlone from './CallToAction/EmailSubscribeModalAlone';
+
+
 export default function FooterCom() {
+  const [open, setOpen] = useState(false); // State to control modal visibility
+
   return (
     <Footer container className='border border-t-8 border-teal-500'>
       <div className='w-full max-w-7xl mx-auto'>
@@ -29,10 +30,12 @@ export default function FooterCom() {
             <div>
               <FooterTitle title="Blog" />
               <FooterLinkGroup col>
+                <FooterLink href="/">Home</FooterLink>
                 <FooterLink href="/about">About Us</FooterLink>
                 <FooterLink href="/contact">Contact Us</FooterLink>
               </FooterLinkGroup>
             </div>
+
             <div>
               <FooterTitle title='Legal' />
               <FooterLinkGroup col>
@@ -53,21 +56,26 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <FooterCopyright
             href='#'
-            by="Consumer's Prices"
+            by="© Consumer’s Prices, All rights reserved"
             year={new Date().getFullYear()}
           />
-          <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
-            <FooterIcon href='#' icon={BsFacebook} />
-            <FooterIcon href='#' icon={BsInstagram} />
-            <FooterIcon href='#' icon={BsTwitter} />
-            <FooterIcon
-              href='https://github.com/sahandghavidel'
-              icon={BsGithub}
-            />
-            <FooterIcon href='#' icon={BsDribbble} />
+          <div className="flex items-center gap-2 mt-4 sm:mt-0 text-sm text-gray-500">
+            <button
+              onClick={() => setOpen(true)}
+              className="flex items-center gap-2 mt-4 sm:mt-0 text-sm text-teal-600 hover:text-teal-800 transition"
+            >
+              <span>Join our newsletter</span>
+              <BsEnvelope size={20} />
+            </button>
           </div>
         </div>
       </div>
+      <Modal show={open} onClose={() => setOpen(false)} size="md" popup>
+        <Modal.Header />
+        <Modal.Body>
+          <EmailSubscribeModalAlone open={open} onClose={() => setOpen(false)} />
+        </Modal.Body>
+      </Modal>
     </Footer>
   );
 }
