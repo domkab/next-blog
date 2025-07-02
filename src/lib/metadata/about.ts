0d070c1bdd31903e0dd218e1279b7e4d1d8d164d
domkab <dominykas.bobinas@gmail.com>
2025-07-02 17:43:53 +0300

    about meta

---
+++ src/lib/metadata/about.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from 'next';
+import { SITE_TITLE } from '../constants';
+
+export const aboutMetadata: Metadata = {
+  title: `About | ${SITE_TITLE}`,
+  description: `Learn more about ${SITE_TITLE} and what we're building.`,
+};
