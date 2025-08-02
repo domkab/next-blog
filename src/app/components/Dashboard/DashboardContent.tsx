9d24b3748bb143aa11e3cf94bd9fb8a00470d367
domkab <dominykas.bobinas@gmail.com>
2025-08-02 18:56:37 +0300

    small dash fix

--- src/app/components/Dashboard/DashboardContent.tsx
+++ src/app/components/Dashboard/DashboardContent.tsx
@@ -32,6 +32,8 @@ export default function DashboardContent() {
     }
   }, [searchParams]);
 
+  if (!isLoaded || !isSignedIn) return null;
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
