4f2a884404310cd2c2855231941954ebe49d2996
domkab <dominykas.bobinas@gmail.com>
2025-08-15 17:24:09 +0300

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,20 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-
-  {
-    rules: {
-      'no-restricted-syntax': [
-        'error',
-        {
-          selector:
-            "JSXAttribute[name.name='src'] Literal[value=/^(?!https?:\\/\\/|\\/).+/]",
-          message:
-            'Public asset paths must start with `/` or `http(s)://` to avoid incorrect resolution.'
-        }
-      ]
-    }
-  }
 ]
 
 export default eslintConfig
