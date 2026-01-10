adbad69e870217e1e73658c815e4eb6804d36152
Dominykas Bobinas <dominykas.bobinas@gmail.com>
2026-01-10 15:29:31 +0200

    new logo + search fix

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Logo() {
+type LogoProps = {
+  width?: number;
+  height?: number;
+};
+
+export default function Logo({ width = 80, height = 64 }: LogoProps) {
   return (
     <Link href="/" className="flex items-center max-h-[30px]">
       <Image
         src="/images/pixel-tech-blog-logo-outfit.svg"
         alt="Pixel Tech Blog Logo"
-        width={130}
-        height={64}
-        className="dark:invert"
+        width={width}
+        height={height}
+        className="dark:brightness-0 dark:invert"
         priority
       />
     </Link>
