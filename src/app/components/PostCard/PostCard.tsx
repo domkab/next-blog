c90aa5ec2699abc13198a8e786a50171128027b3
Dominykas Bobinas <dominykas.bobinas@kilo.health>
2026-02-21 06:13:45 +0200

    add post card sry

---
+++ src/app/components/PostCard/PostCard.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import { PostType } from "@/types/Post";
+import Image from "next/image";
+import { getImageUrl } from "@/utils/getImageUrl";
+import { labelFromSlug } from "@/utils/generateSlug";
+import styles from "./PostCard.module.scss";
+import clsx from "clsx";
+
+type PostCardProps = {
+  post: PostType;
+  limit?: number;
+};
+
+export default function PostCard({ post }: PostCardProps) {
+  return (
+    <div
+      className={clsx(
+        "group relative w-full border md:min-w-[320px] border-teal-500 hover:border-2 h-[420px] overflow-hidden rounded-lg sm:w-full transition-all",
+        styles.postCard,
+      )}
+    >
+      <Link href={`/post/${post.slug}`}>
+        <div
+          className={clsx(
+            "relative w-full h-[260px] group-hover:h-[200px] transition-all duration-300",
+          )}
+        >
+          <figure>
+            <Image
+              src={getImageUrl(post.images.main.url)}
+              alt="post cover"
+              fill
+              unoptimized
+              priority
+              className={clsx("object-cover z-20", styles.postCard__image)}
+            />
+          </figure>
+        </div>
+      </Link>
+
+      <div className="p-3 flex flex-col gap-2">
+        <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
+
+        <div className="flex justify-between mb-3">
+          <span className="italic text-sm/4">
+            {labelFromSlug(post.category)}
+          </span>
+          <span className="italic text-sm/4">
+            {post && new Date(post.createdAt).toLocaleDateString()}
+          </span>
+        </div>
+
+        <p className="text-sm">{post.description}</p>
+        <Link
+          href={`/post/${post.slug}`}
+          className={clsx(
+            "z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2",
+          )}
+        >
+          Read article
+        </Link>
+      </div>
+    </div>
+  );
+}
