70e592e912f7d15225c473f739a61a52a8c60d15
Dominykas Bobinas <dominykas.bobinas@kilo.health>
2026-02-22 12:04:12 +0200

    logo now can take in custom classNames

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,20 +1,22 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
+import clsx from 'clsx';
 
 type LogoProps = {
   width?: number;
   height?: number;
+  className?: string;
 };
 
-export default function Logo({ width = 80, height = 64 }: LogoProps) {
+export default function Logo({ width = 80, height = 60, className }: LogoProps) {
   return (
-    <Link href="/" className="flex items-center max-h-[30px]">
+    <Link href="/" className="flex items-center">
       <Image
         src="/images/pixel-tech-blog-logo-outfit.svg"
         alt="Pixel Tech Blog Logo"
         width={width}
         height={height}
-        className="dark:brightness-0 dark:invert"
+        className={clsx(className, "dark:brightness-0 dark:invert")}
         priority
       />
     </Link>
