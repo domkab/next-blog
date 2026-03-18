5000467336e11c6ec43a3462d34f44412f310de2
Dominykas Bobinas <dominykas.bobinas@gmail.com>
2026-03-18 22:01:20 +0200

    post content empty p now not 0 margin

--- src/app/components/PostContent/PostContent.tsx
+++ src/app/components/PostContent/PostContent.tsx
@@ -111,15 +111,20 @@ const PostContent: React.FC<PostContentProps> = ({ post }) => {
           );
         }
 
-        const isEmptyParagraph = p.children.every(c => {
-          if (c.type === "text") {
-            const text = (c as DomText).data.replace(/\u00A0/g, " ");
-            return text.trim() === "";
-          }
-          // Quill sometimes uses <br> inside empty paragraphs
-          if (c.type === "tag" && (c as DomElement).name === "br") return true;
-          return false;
-        });
+        const isEmptyParagraph =
+          p.children.length === 0 ||
+          p.children.every(c => {
+            if (c.type === "text") {
+              const text = (c as DomText).data.replace(/\u00A0/g, " ");
+              return text.trim() === "";
+            }
+            // Quill sometimes uses <br> inside empty paragraphs
+            if (c.type === "tag" && (c as DomElement).name === "br") {
+              return true;
+            }
+
+            return false;
+          });
 
         if (isEmptyParagraph) {
           return (
