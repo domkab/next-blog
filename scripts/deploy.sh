42819855dfb288110541b51f315768280443bbd5
Dominykas Bobinas <dominykas.bobinas@gmail.com>
2026-04-12 23:21:40 +0300

    delete deploy.sh

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "🔁 Cloning repo..."
-git clone https://github.com/domkab/next-blog.git
-cd next-blog || exit 1
-
-echo "📦 Building and launching containers (app + Caddy)..."
-docker compose up -d --build nextblog caddy
-
-echo "✅ App should now be live at: https://laurynogargasoapiserver.xyz"
