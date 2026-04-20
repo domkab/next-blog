8571e96ba078eb11a6ab729bc74ebf3fe2f88a67
Dominykas Bobinas <dominykas.bobinas@gmail.com>
2026-04-20 21:57:17 +0300

    update post model

--- src/lib/models/postModel.ts
+++ src/lib/models/postModel.ts
@@ -11,6 +11,15 @@ const InlineImageSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    storagePath: {
+      type: String,
+      default: "",
+    },
+    provider: {
+      type: String,
+      enum: ["firebase"],
+      default: undefined,
+    },
     meta: {
       author: { type: String, default: "" },
       description: { type: String, default: "" },
@@ -50,6 +59,15 @@ const postSchema = new mongoose.Schema(
           type: String,
           default:
             "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png",
+        },
+        storagePath: {
+          type: String,
+          default: "",
+        },
+        provider: {
+          type: String,
+          enum: ["firebase"],
+          default: undefined,
         },
         meta: {
           author: { type: String, default: "" },
