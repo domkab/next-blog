2a314bc314590223a2410d90e6831b472ccd2a5e
Dominykas Bobinas <dominykas.bobinas@gmail.com>
2026-05-10 13:54:00 +0300

    format

--- src/lib/mongodb/mongoose.ts
+++ src/lib/mongodb/mongoose.ts
@@ -1,22 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let initialized = false;
+// let initialized = false;
 const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DBNAME;
 
 if (!uri || !dbName) {
-  throw new Error('MongoDB URI or DB name is not defined in the environment variables');
+  throw new Error(
+    "MongoDB URI or DB name is not defined in the environment variables",
+  );
+}
+
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 };
 
 export const connect = async () => {
-  mongoose.set('strictQuery', true);
+  mongoose.set("strictQuery", true);
 
   if (!uri) {
-    throw new Error('MongoDB URI is not defined in the environment variables');
+    throw new Error("MongoDB URI is not defined in the environment variables");
   }
 
   if (initialized) {
-    console.log('already connected to MongoDB');
+    console.log("already connected to MongoDB");
 
     return;
   }
@@ -26,9 +33,8 @@ export const connect = async () => {
       dbName: dbName,
     });
 
-    console.log('Connected to MongoDb')
+    console.log("Connected to MongoDb");
     initialized = true;
-
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(`Database connection error: ${error}`);
@@ -36,4 +42,4 @@ export const connect = async () => {
       throw new Error(`Database connection error: ${String(error)}`);
     }
   }
-}
+};
