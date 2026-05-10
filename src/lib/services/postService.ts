e7418f8e68fa2d1c838c71406424d3f1997c43a2
Dominykas Bobinas <dominykas.bobinas@gmail.com>
2026-05-10 13:18:40 +0300

    create category utils file

--- src/lib/services/postService.ts
+++ src/lib/services/postService.ts
@@ -3,36 +3,7 @@ import { PostType } from "@/types/Post";
 import Post from "../models/postModel";
 import { Types } from "mongoose";
 import FeaturedPost from "../models/featuredPostModel";
-import Category from "@/lib/models/CategoryModel";
-
-export type PostWithCategoryName = PostType & {
-  categoryName: string | null;
-};
-
-async function withCategoryNames(
-  posts: PostType[],
-): Promise<PostWithCategoryName[]> {
-  const slugs = Array.from(
-    new Set(posts.map(p => p?.category).filter(Boolean) as string[]),
-  );
-
-  if (slugs.length === 0) {
-    return posts.map(p => ({ ...p, categoryName: null }));
-  }
-
-  const categories = await Category.find({ slug: { $in: slugs } })
-    .select({ slug: 1, name: 1, _id: 0 })
-    .lean();
-
-  const categoryMap = new Map<string, string>(
-    categories.map(c => [c.slug, c.name]),
-  );
-
-  return posts.map(p => ({
-    ...p,
-    categoryName: p.category ? categoryMap.get(p.category) ?? null : null,
-  }));
-}
+import { PostWithCategoryName, withCategoryNames } from './postServiceUtils';
 
 export async function getRecentPosts(
   limit = 9,
